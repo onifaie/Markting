f0921f8fa1085bb732d5f1a0fa2ebbf2f5b80f9d
obeid AL-otibi <31520330+onifaie@users.noreply.github.com>
2021-06-25 02:31:43 +0300

    add_productModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    """Model definition for product."""
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for product."""
+
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        """Unicode representation of product."""
+        pass
